8d0902cd537df168dd853c468fcbd64c53db0151
tqcss <134121905+tqcss@users.noreply.github.com>
2023-11-09 00:46:37 +0800

    fix: order assigner refresh after completing order

--- game-dev-gauntlet-entry/Assets/Scripts/RecipeManager.cs
+++ game-dev-gauntlet-entry/Assets/Scripts/RecipeManager.cs
@@ -71,7 +71,7 @@ public class RecipeManager : MonoBehaviour
             failPlayer();
         }
         orderManager.timerRunning = false;
-
+        orderManager.currentOrderPrompt = null;
         destroyAllLooseItems();
     }
 
