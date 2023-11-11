e9d1072a0b833e0dbbc68994e882c1fda61749ff
tqcss <134121905+tqcss@users.noreply.github.com>
2023-11-11 22:25:03 +0800

    feat : partial rework to audio system

---
+++ game-dev-gauntlet-entry/Assets/AudioManager.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Dictionary<string, AudioClip> audioClips = new Dictionary<string, AudioClip>();
+    private List<AudioSource> audioQueue = new List<AudioSource>(4);
+
+    private void Awake()
+    {
+        foreach(AudioClip audioClip in Resources.LoadAll<AudioClip>("Audio").ToArray())
+        {
+            audioClips.Add(audioClip.name, audioClip);
+        }
+    }
+
+    public void StopAudio(AudioSource audioSource)
+    {
+        audioQueue.Remove(audioSource);
+        audioSource.Stop();
+        Destroy(audioSource);
+    }
+
+    public AudioSource playBackgroundMusic(AudioClip audioClip, float fadeDuration, float targetVolume, float startTimeStamp)
+    {
+        AudioSource newAudioSource = gameObject.AddComponent<AudioSource>();
+        newAudioSource.clip = audioClip;
+        newAudioSource.time = startTimeStamp;
+        newAudioSource.loop = true;
+
+        StartCoroutine(FadeIn(newAudioSource, fadeDuration, targetVolume));
+        return newAudioSource;
+    }
+
+    public void playAudio(AudioClip audioClip, float startTimestamp)
+    {
+        AudioSource newAudioSource = gameObject.AddComponent<AudioSource>();
+        newAudioSource.clip = audioClip;
+        newAudioSource.time = startTimestamp;
+
+        if (audioQueue.Count == audioQueue.Capacity)
+        {
+            AudioSource firstInQueue = audioQueue[0];
+            audioQueue.RemoveAt(0);
+            Destroy(firstInQueue);
+        }
+        audioQueue.Add(newAudioSource);
+    }
+
+    public AudioSource FadeInAudio(AudioClip audioClip, float fadeDuration, float targetVolume)
+    {
+        AudioSource newAudioSource = gameObject.AddComponent<AudioSource>();
+        newAudioSource.clip = audioClip;
+
+        if (audioQueue.Count == audioQueue.Capacity)
+        {
+            AudioSource firstInQueue = audioQueue[0];
+            audioQueue.RemoveAt(0);
+            Destroy(firstInQueue);
+        }
+        audioQueue.Add(newAudioSource);
+
+        StartCoroutine(FadeIn(newAudioSource, fadeDuration, targetVolume));
+        return newAudioSource;
+    }
+
+    public AudioSource FadeInAudio(AudioClip audioClip, float fadeDuration, float targetVolume, float startTimestamp)
+    {
+        AudioSource newAudioSource = gameObject.AddComponent<AudioSource>();
+        newAudioSource.clip = audioClip;
+        newAudioSource.time = startTimestamp;
+
+        StartCoroutine(FadeIn(newAudioSource, fadeDuration, targetVolume));
+        return newAudioSource;
+    }
+
+    public void FadeOutAudio(AudioSource audioSource, float fadeDuration)
+    {
+        StartCoroutine(FadeOut(audioSource, fadeDuration));
+    }
+
+    IEnumerator FadeIn(AudioSource audioSource, float fadeDuration, float targetVolume)
+    {
+        float currentTime = 0;
+        float startVolume = 0;
+
+        while (currentTime < fadeDuration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(startVolume, targetVolume, currentTime / fadeDuration);
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOut(AudioSource audioSource, float fadeDuration)
+    {
+        float currentTime = 0;
+        float startVolume = audioSource.volume;
+
+        while (currentTime < fadeDuration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(startVolume, 0, currentTime / fadeDuration);
+            yield return null;
+        }
+
+        audioSource.Stop(); // kailangan pa ba to?
+        Destroy(audioSource);
+    }
+}
